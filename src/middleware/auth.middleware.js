16ebcdb58c950c1ccb4d1b9865208499c4207ab4
JPTirso <jptirso2@gmail.com>
2026-06-27 16:42:42 -0300

    Arrumando o middleware auth

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,9 +4,9 @@ function auth (req,res,next){
     if(!authHeader){
         return res.status(400).json({message: "Token não informado"})
     }
-    const token = authHeader.split(" ")[1]
+    const acessToken = authHeader.split(" ")[1]
     try{
-        const decoded = jwt.verify(token, process.env.SECRET)
+        const decoded = jwt.verify(acessToken, process.env.ACESS_SECRET)
         req.userId = decoded.id
         req.userRole = decoded.role
 
